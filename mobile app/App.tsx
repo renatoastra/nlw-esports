1d0ed9fb4dcc5c97072e89817fc8cdeb7605a836
Renato Costa <renato@qualyteam.com.br>
2022-09-12 00:17:39 -0300

    Day one - corrigindo problemas de commit no mobile

---
+++ mobile app/App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Button title='Toque' />
+      <Button title='Toque 2' />
+      <Button title='Toque 3' />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string,
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity >
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+// teste aaa
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
