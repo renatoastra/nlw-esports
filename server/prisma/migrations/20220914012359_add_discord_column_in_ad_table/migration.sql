749958c4854e2597ac752d4a8e927d8a0f22db0d
Renato Costa <renato@qualyteam.com.br>
2022-09-14 00:11:57 -0300

    Day three - Criacao do back-end

---
+++ server/prisma/migrations/20220914012359_add_discord_column_in_ad_table/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - Added the required column `discord` to the `Ad` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Ad" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "gameId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "yearsPlaying" INTEGER NOT NULL,
+    "discord" TEXT NOT NULL,
+    "weekDays" TEXT NOT NULL,
+    "hourStart" INTEGER NOT NULL,
+    "hourEnd" INTEGER NOT NULL,
+    "useVoiceChannel" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Ad_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Ad" ("createdAt", "gameId", "hourEnd", "hourStart", "id", "name", "useVoiceChannel", "weekDays", "yearsPlaying") SELECT "createdAt", "gameId", "hourEnd", "hourStart", "id", "name", "useVoiceChannel", "weekDays", "yearsPlaying" FROM "Ad";
+DROP TABLE "Ad";
+ALTER TABLE "new_Ad" RENAME TO "Ad";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
